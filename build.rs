a4a121d5e6a2cb6717c912a80c6ac8e36f792745
Josh Stoik <josh@blobfolio.com>
2022-04-17 00:11:29 -0700

    misc: pre-compile extension constant in build.rs

---
+++ build.rs
@@ -0,0 +1,36 @@
+/*!
+# Yesvgmap: Build
+*/
+
+use dowser::Extension;
+use std::{
+	fs::File,
+	io::Write,
+	path::Path,
+};
+
+
+
+/// # Build.
+///
+/// We might as well pre-compile the extensions we're looking for.
+pub fn main() {
+	println!("cargo:rerun-if-env-changed=CARGO_PKG_VERSION");
+
+	let out = format!(
+		r"const E_SVG: Extension = {};",
+		Extension::codegen(b"svg"),
+	);
+
+	let out_path = std::fs::canonicalize(std::env::var("OUT_DIR").expect("Missing OUT_DIR."))
+		.expect("Missing OUT_DIR.")
+		.join("yesvgmap-extensions.rs");
+
+	write(&out_path, out.as_bytes());
+}
+
+/// # Write File.
+fn write(path: &Path, data: &[u8]) {
+	File::create(path).and_then(|mut f| f.write_all(data).and_then(|_| f.flush()))
+		.expect("Unable to write file.");
+}
